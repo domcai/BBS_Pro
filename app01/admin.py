345a178e93021381b2bf6bd714b5a4586d2d36b3
luo1fly <luo1fly@gmail.com>
2015-03-19 14:29:19 +0800

    django admin

--- app01/admin.py
+++ app01/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from app01.models import BBS,Category,BBS_user
 # Register your models here.
 
-admin.site.register(BBS)
+class BBS_Admin(admin.ModelAdmin):
+    list_display = ('title','summary','author','view_count','create_at',)
+    list_filter = ('create_at',)
+    search_fields = ('title','author__user__username',)
+admin.site.register(BBS,BBS_Admin)
 admin.site.register(BBS_user)
 admin.site.register(Category)
