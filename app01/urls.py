696ffbcdf6a1b062b87837a1c40be17a2f825051
luo1fly <luo1fly@gmail.com>
2015-03-23 20:02:53 +0800

    commit 3rd

--- app01/urls.py
+++ app01/urls.py
@@ -4,5 +4,6 @@ from app01 import views
 
 urlpatterns = patterns('',
     url(r'^detail/(\d+)/$', views.bbs_detail),
+    url(r'^sub_comment/$', views.sub_comment),
     url(r'',views.index),
 )
--- app01/views.py
+++ app01/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 import models
+from django.http import HttpResponseRedirect
+#from django.template.context import RequestContext
 
 # Create your views here.
 
@@ -12,3 +14,9 @@ def bbs_detail(request,bbs_id):
     bbs = models.BBS.objects.get(id=bbs_id)
     #print 'hello bbs_details'
     return render_to_response('bbs_detail.html',{'bbs_details':bbs})
+
+
+def sub_comment(request):
+    print request.POST
+    bbs_id = request.POST.get('bbs_id')
+    return HttpResponseRedirect('/detail/%s/'% bbs_id)
